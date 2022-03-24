53e450d6d7ba9324afca53967b47bed2c381b7fa
CaDiBob <93527781+CaDiBob@users.noreply.github.com>
2022-03-24 18:22:16 +0900

    Добавил кнопки в бота ВКонтакте

--- vk_bot.py
+++ vk_bot.py
@@ -1,27 +1,46 @@
+import redis
+
 import vk_api as vk
 from vk_api.longpoll import VkLongPoll, VkEventType
+from vk_api.keyboard import VkKeyboard, VkKeyboardColor
 from vk_api.utils import get_random_id
 from environs import Env
+from db import get_connect_db
 
 
-def send_reply_message(event, vk_api):
+def handle_new_question_request(event, vk_api, keyboard):
+    db = context.bot_data['db']
+    question = random.choice(list(
+        context.bot_data['questions']
+    ))
+    update.message.reply_text(question, reply_markup=get_buttons())
+    db.set(update.message.chat_id, question)
+
+
+def send_reply_message(event, vk_api, keyboard):
     vk_api.messages.send(
         user_id=event.user_id,
-        message=event.text,
-        random_id=get_random_id()
+        message='Привет я бот для викторин что бы начать нажми "Новый вопрос".',
+        random_id=get_random_id(),
+        keyboard=keyboard.get_keyboard()
     )
 
 
 def main():
     env = Env()
     env.read_env('.env')
+    db = get_connect_db()
     vk_token = env('VK_TOKEN')
     vk_session = vk.VkApi(token=vk_token)
     vk_api = vk_session.get_api()
+    keyboard = VkKeyboard()
+    keyboard.add_button('Новый вопрос', color=VkKeyboardColor.PRIMARY)
+    keyboard.add_button('Сдаться')
     longpoll = VkLongPoll(vk_session)
     for event in longpoll.listen():
         if event.type == VkEventType.MESSAGE_NEW and event.to_me:
-            send_reply_message(event, vk_api)
+            if event.text.lower() == 'start':
+                send_reply_message(event, vk_api, keyboard)
 
 
 if __name__ == "__main__":
