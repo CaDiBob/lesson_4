91db2f8df6cdabf84653970ffaafe6ab8cfe8367
CaDiBob <93527781+CaDiBob@users.noreply.github.com>
2022-03-24 20:05:10 +0900

    Общее
    -Добавил ответы на вопросы
    -Добавил функцию сдаться

--- vk_bot.py
+++ vk_bot.py
@@ -1,29 +1,86 @@
-import redis
+import random
 
+from time import sleep
 import vk_api as vk
 from vk_api.longpoll import VkLongPoll, VkEventType
 from vk_api.keyboard import VkKeyboard, VkKeyboardColor
 from vk_api.utils import get_random_id
 from environs import Env
 from db import get_connect_db
+from questions import get_questions_quiz
 
 
-def handle_new_question_request(event, vk_api, keyboard):
-    db = context.bot_data['db']
+def get_answer(event, db):
+    questions = get_questions_quiz()
+    question = db.get(event.user_id)
+    answer, *trash = questions[question].split('.')
+    return answer.strip()
+
+
+def get_give_up(event, vk_api, db):
+    answer = get_answer(event, db)
+    vk_api.messages.send(
+        user_id=event.user_id,
+        message=f'Ответ: {answer}',
+        random_id=get_random_id(),
+        keyboard=get_keyboard(),
+    )
+    sleep(1)
+    vk_api.messages.send(
+        user_id=event.user_id,
+        message='Новый вопрос:',
+        random_id=get_random_id(),
+        keyboard=get_keyboard(),
+    )
+    handle_new_question_request(event, vk_api, db)
+
+
+def handle_solution_attempt(event, vk_api, db):
+    answer = get_answer(event, db)
+    text = event.text
+    if text == answer:
+        vk_api.messages.send(
+            user_id=event.user_id,
+            message='Правильно! Поздравляю! Для следующего вопроса нажми «Новый вопрос»',
+            random_id=get_random_id(),
+            keyboard=get_keyboard()
+        )
+    if text != answer:
+        vk_api.messages.send(
+            user_id=event.user_id,
+            message='Неправильно… Попробуешь ещё раз?',
+            random_id=get_random_id(),
+            keyboard=get_keyboard()
+        )
+
+
+def handle_new_question_request(event, vk_api, db):
     question = random.choice(list(
-        context.bot_data['questions']
+        get_questions_quiz()
     ))
-    update.message.reply_text(question, reply_markup=get_buttons())
-    db.set(update.message.chat_id, question)
+    vk_api.messages.send(
+        user_id=event.user_id,
+        message=question,
+        random_id=get_random_id(),
+        keyboard=get_keyboard()
+    )
+    db.set(event.user_id, question)
 
 
-def send_reply_message(event, vk_api, keyboard):
+def start(event, vk_api):
     vk_api.messages.send(
         user_id=event.user_id,
         message='Привет я бот для викторин что бы начать нажми "Новый вопрос".',
         random_id=get_random_id(),
-        keyboard=keyboard.get_keyboard()
+        keyboard=get_keyboard()
     )
+
+
+def get_keyboard():
+    keyboard = VkKeyboard()
+    keyboard.add_button('Новый вопрос', color=VkKeyboardColor.PRIMARY)
+    keyboard.add_button('Сдаться')
+    return keyboard.get_keyboard()
 
 
 def main():
@@ -33,14 +90,17 @@ def main():
     vk_token = env('VK_TOKEN')
     vk_session = vk.VkApi(token=vk_token)
     vk_api = vk_session.get_api()
-    keyboard = VkKeyboard()
-    keyboard.add_button('Новый вопрос', color=VkKeyboardColor.PRIMARY)
-    keyboard.add_button('Сдаться')
     longpoll = VkLongPoll(vk_session)
     for event in longpoll.listen():
         if event.type == VkEventType.MESSAGE_NEW and event.to_me:
             if event.text.lower() == 'start':
-                send_reply_message(event, vk_api, keyboard)
+                start(event, vk_api)
+            elif event.text == 'Новый вопрос':
+                handle_new_question_request(event, vk_api, db)
+            elif event.text == 'Сдаться':
+                get_give_up(event, vk_api, db)
+            else:
+                handle_solution_attempt(event, vk_api, db)
 
 
 if __name__ == "__main__":
