c92753e15a5888b77112feb01c9209679790a9dd
CaDiBob <93527781+CaDiBob@users.noreply.github.com>
2022-03-27 08:06:59 +0900

    Update vk_bot.py

--- vk_bot.py
+++ vk_bot.py
@@ -93,7 +93,6 @@ def main():
         port=env('VK_REDIS_PORT'),
         password=env('VK_REDIS_PASSWORD'),
         decode_responses=True,
-        db=0,
     )
     vk_token = env('VK_TOKEN')
     vk_session = vk.VkApi(token=vk_token)
